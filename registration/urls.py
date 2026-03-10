609e0f0aea967db26c29f3a3be2c571d57ea6fdb
Boniface Masila <dietboni@gmail.com>
2026-03-10 15:18:09 +0300

    `Added features and about views and updated templates to link to them`

--- registration/urls.py
+++ registration/urls.py
@@ -7,6 +7,8 @@ from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, 
 urlpatterns = [
     path('', landing, name='landing'),
     path('home/', home, name='home'),
+    path('features/', features, name='features'),
+    path('about/', about, name='about'),
     path('sign_up/', sign_up, name='sign_up'),
     path('sign_in/', sign_in, name='sign_in'),
     path('sign_out/', sign_out, name='sign_out'),
